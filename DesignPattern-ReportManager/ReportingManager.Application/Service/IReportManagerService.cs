03d5b9d0138ac0c8dd7c5b19413f91bb78d2b932
Andre <andre.bizetto@gmail.com>
2025-01-31 15:28:27 -0300

    setup application project - service layer

---
+++ DesignPattern-ReportManager/ReportingManager.Application/Service/IReportManagerService.cs
@@ -0,0 +1,5 @@
+public interface IReportManagerService
+{
+    string GenerateReport();
+    void PrintReport();
+}
--- DesignPattern-ReportManager/ReportingManager.Presentation/Program.cs
+++ DesignPattern-ReportManager/ReportingManager.Presentation/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ReportingManager
 {
@@ -12,13 +13,28 @@ namespace ReportingManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(".........");
-            Console.WriteLine(">>> END");
-            Console.WriteLine("......... Test 101");
-            ReportManagerBO bo = new ReportManagerBO(1,"My Report","andre");
-            Console.WriteLine(bo.GenerateReport());
+            //PHASE 1: Basic Steps
+            Console.WriteLine("....................");
+            Console.WriteLine("... " + DateTime.Now);
+            ReportManagerService service = new ReportManagerService(1,"My Report","andre");
+            Console.WriteLine(service.GenerateReport());
+            Console.WriteLine("....................");
 
-            //var input = Console.ReadKey();
+
+            //PHASE 2:  DI Steps
+            // Set up the dependency injection container
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IReportManagerService, ReportManagerService>() // Registering the service
+                
+                .BuildServiceProvider();
+
+            // Resolving the ReportManager instance
+            var reportManager = serviceProvider.GetService<IReportManagerService>();
+            var result = reportManager.GenerateReport(); // Calling the method to generate the report
+            Console.WriteLine(result);
+
+
+            Console.WriteLine(".........END...........");
         }
     }
 }
